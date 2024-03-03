dd5dc29492be61d99fa0aebd57b079b41b9af7ef
rimasultana <rimasultana20225@gmail.com>
2024-03-03 22:20:17 +0600

    Data

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const d = new Date("2021-03-25");
+// const d = new Date("2021-03-25");
 
-document.getElementById("demo").innerHTML = d.getMonth() + 1;
+// document.getElementById("demo").innerHTML = d.getMonth() + 1;
+
+const months = ["January","February","March","April","May","June",
+"July","August","September","October","November","December"];
+
+const d = new Date("2021-03-25");
+let month = months[d.getMonth()];
+document.getElementById("demo").innerHTML = month;
